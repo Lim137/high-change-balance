5ffe5c55f12fb8807481ea117487c2e5b27d92e2
Lim137 <sergei.ignatov2001@inbox.ru>
2024-07-18 22:07:29 +0300

    added getaddr route

--- cmd/balance-tracker/main.go
+++ cmd/balance-tracker/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"balance-tracker/internal/app"
+	"balance-tracker/pkg/env"
+	"log"
+	"os"
+)
 
+func main() {
+	err := env.LoadEnvFile(".env")
+	if err != nil {
+		log.Fatalf("ошибка загрузки .env файла: %s", err)
+	}
+	port := os.Getenv("PORT")
+	app.New(port)
+	//value1 := os.Getenv("API_KEY")
+	//value2 := os.Getenv("TEST")
+	//fmt.Println("API_KEY:", value1)
+	//fmt.Println("TEST:", value2)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,15 @@
 package app
+
+import (
+	high_change_balance "balance-tracker/internal/handlers/high-change-balance"
+	"log"
+	"net/http"
+)
+
+func New(port string) {
+	const op = "app.New"
+	http.HandleFunc("/getaddr", high_change_balance.BalanceTrackerHandler)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("ошибка при запуске сервера: ", err)
+	}
+}
